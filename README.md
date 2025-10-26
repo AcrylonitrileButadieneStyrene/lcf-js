8800c62b434d61d5153303c28ff21d5db4c47149
AcrylonitrileButadieneStyrene <AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-10-26 02:42:41 +0000

    deploy: 64cb057a5931deff432ecffe1f3fa19aa60a19e8

---
+++ README.md
@@ -0,0 +1,2 @@
+# lcf-js
+JavaScript bindings to [lcf-rs](https://github.com/AcrylonitrileButadieneStyrene/lcf-rs).
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { Lcf } from './generated.d.ts';
+export type * from './generated.d.ts';
+export declare function parse(bytes: Uint8Array<ArrayBufferLike>): Lcf;
